404f673b398a607229a301dbb6f015fd812c9339
nathankw <nathankw@stanford.ed>
2015-06-30 12:15:42 -0700

    Added samtools/1.2 module in peakseq.form_sample_files() to be used in all commands in the sjm file whose names end in _All_merge.

--- peakseq.py
+++ peakseq.py
@@ -104,7 +104,7 @@ def form_sample_files(name, sample):
 	for rep in sample.replicates:
 		jobs.append(sjm.Job(rep.rep_name(sample) + '_merge', form_replicate_files(rep, sample), queue=QUEUE, project=PROJECT,sched_options="-m e"))
 	
-	jobs.append(sjm.Job(sample.run_name + '_All_merge', form_replicate_files(sample.combined_replicate, sample), queue=QUEUE, project=PROJECT,sched_options="-m e"))
+	jobs.append(sjm.Job(sample.run_name + '_All_merge', form_replicate_files(sample.combined_replicate, sample),modules=["samtools/1.2"], queue=QUEUE, project=PROJECT,sched_options="-m e"))
 	sample.add_jobs(name, jobs)
 				
 def form_replicate_files(rep, sample):
